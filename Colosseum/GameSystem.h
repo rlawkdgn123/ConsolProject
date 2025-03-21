be1a440210f8b14f6876c5fb45c6a014b4cd38da
Cho-hs0004 <huntmd0004@gmail.com>
2025-03-21 17:04:39 +0900

    1

--- Colosseum/GameSystem.h
+++ Colosseum/GameSystem.h
@@ -20,11 +20,28 @@ namespace global
     }
 }
 
+typedef struct {
+    char* skillName;
+    int skillDamage;
+}SKILL;
+
+typedef struct {
+    char* itemName;
+    int healing = 0;
+    int stun = 0;
+}ITEM;
+
+typedef struct {
+    int hp;
+    int attackDamage;
+    SKILL skill[4];
+}PLAYER;
+
 void StartGame();
 void Clamp(short& n, short min, short max); // 레퍼런스 타입에 대해 배워 봅시다.
 void DrawPlayer();
 void DrawEnemy();
-void UpdatePlayerPosition();
+void UpdateCussorPosition();
 void UpdatePlayer();
 void UpdateEnemy();
 void EndGame();
