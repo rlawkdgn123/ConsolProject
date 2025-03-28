fa6467f0e1fd7c85eb976ffeb92949772c6fe808
Cho-hs0004 <huntmd0004@gmail.com>
2025-03-28 11:29:45 +0900

    1

--- Colosseum/CharacterLocal.h
+++ Colosseum/CharacterLocal.h
@@ -7,3 +7,11 @@ const char* BerserkerIdle[23] = {
             ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (16).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (17).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (18).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (19).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (20).txt",
             ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (21).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (22).txt", ".\\Images\\Heros\\Ascii\\Berserker\\IDLE\\Berserker_IDLE (23).txt"
 };
+
+const char* ArcherIdle[24] = {
+            ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (1).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (2).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (3).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (4).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (5).txt",
+            ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (6).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (7).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (8).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (9).txt" , ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (10).txt",
+            ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (11).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (12).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (13).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (14).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (15).txt",
+            ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (16).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (17).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (18).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (19).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (20).txt",
+            ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (21).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (22).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (23).txt", ".\\Images\\Heros\\Ascii\\Archer\\IDLE\\Archer_IDLE (24).txt"
+};
